200aad131967f0fc55ff44c90685b0e361eeb2a8
Andy Eschbacher <otto.hasselblad@gmail.com>
2013-07-19 18:39:20 -0400

    Transitioned from header files to definitions in cpp files

--- src/rr.cpp
+++ src/rr.cpp
@@ -25,53 +25,63 @@
  *	Change rrcntr++ in the if statement in "Applying Threshold" to rrtemp++ instead
  */
 
-//double rr(double *__restrict__ x, double *__restrict__ y, \
-//		  int &__restrict__ rrcntr)
-//{
-//	extern int globalWindow, globalOverlap;
-//	extern double ge;
-//	double dx, dy, dmax;
-//	int RR = rrcntr;	// Transfer previous overlap into current count
-//	rrcntr = 0;			// Reset overlap for this window
-////	int rrtemp = 0;
-//	static int diff = globalWindow - globalOverlap;
-//	int i, j;
-//	static double wsquared = static_cast<double> (globalWindow*globalWindow);
-//
-////#pragma omp parallel for default(none) private(dx,dy,dmax,i,j,chx,chy) \
-////shared(x,y,diff) schedule(guided) reduction(+:RR,rrtemp)
-//	for (i = globalOverlap; i < globalWindow; i++)
-//	{
-//		for (j = 0; j < i; j++)
-//		{
-//			// Calculate recurrences; check if they are on opposite edges
-//			if ( (x[i] > (1.0 - ge) && x[j] < ge) )
-//				dx = fabs(1.0 - x[i] + x[j]);
-//			else
-//				dx = fabs(x[i] - x[j]);
-//
-//			if ( (y[i] > (1.0 - ge) && y[j] < ge) )
-//				dy = fabs(1.0 - y[i] + y[j]);
-//			else
-//				dy = fabs(y[i] - y[j]);
-//
-//			// Maximum Norm
-//			dx > dy ? (dmax = dx) : (dmax = dy);
-//
-//			// Apply Threshold
-//			if (dmax < ge)
-//			{
-//				RR++;
-//				if ( (i > diff) && (j >= diff) )
-//					rrcntr++;
-//			}
-//		}
-//	} /*** End of Parallel Section ***/
-//
-////	rrcntr = rrtemp;
-//
-//	return ( double(2*RR + globalWindow) / wsquared );
-//}
+/* TODO: find out if declararing some values as volatile will help speed
+ * up the program
+ */
+double rr(double *__restrict__ x, double *__restrict__ y, \
+		  int &__restrict__ rrcntr)
+{
+	extern int globalWindow, globalOverlap;
+	extern double ge;
+	double dx, dy, dmax;
+	int RR = rrcntr;	// Transfer previous overlap into current count
+	rrcntr = 0;			// Reset overlap for this window
+//	int rrtemp = 0;
+	static int diff = globalWindow - globalOverlap;
+	int i, j;
+	static double wsquared = static_cast<double> (globalWindow*globalWindow);
+
+//#pragma omp parallel for default(none) private(dx,dy,dmax,i,j,chx,chy) \
+//shared(x,y,diff) schedule(guided) reduction(+:RR,rrtemp)
+	for (i = globalOverlap; i < globalWindow; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			// Calculate recurrences; check if they are on opposite edges
+			if ( (x[i] > (1.0 - ge) && x[j] < ge) )
+				dx = fabs(1.0 - x[i] + x[j]);
+			else
+				dx = fabs(x[i] - x[j]);
+
+			if ( (y[i] > (1.0 - ge) && y[j] < ge) )
+				dy = fabs(1.0 - y[i] + y[j]);
+			else
+				dy = fabs(y[i] - y[j]);
+
+			// Maximum Norm
+			dx > dy ? (dmax = dx) : (dmax = dy);
+
+			// Apply Threshold
+			if (dmax < ge)
+			{
+				RR++;
+				if ( (i > diff) && (j >= diff) )
+					rrcntr++;
+			}
+		}
+	} /*** End of Parallel Section ***/
+
+//	rrcntr = rrtemp;
+
+	return ( double(2*RR + globalWindow) / wsquared );
+    /* many calculations could be avoided if threshold is redefined as renormed 
+     value = (thrsOld * wsquared - globalWindow) / 2.0
+     If 10^6 runs through rr are given, then 4x as many calcs can be avoided
+     (including the double() cast) and rr() could return an int...
+     While this may speed up the program some, it would also be harder to under-
+     stand.
+     */
+}
 
 /*while (k<w/(w-n) && i > w-diff && j >= w-diff) {	// rough idea on how to implement 
  andy												// the multiple window-overlap
