f3b02b65c8ad5f423116c12fb093d2e98ec22a86
Andy Eschbacher <otto.hasselblad@gmail.com>
2013-07-23 09:35:53 -0400

    Added test for misc/logspace

--- test/test_misc_cpp.cpp
+++ test/test_misc_cpp.cpp
@@ -1,8 +1,11 @@
 // Compile with g++ -otest -lboost_unit_test_framework TestExample.cpp -L/opt/local/lib -I/opt/local/include
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE Hint test
+#define BOOST_TEST_MODULE rrstdmp test
 #include <boost/test/unit_test.hpp>
-#include "testlib.h"
+#include <cmath>
+
+// Library to be tested
+#include "misc.h"
 
 
 BOOST_AUTO_TEST_CASE( test1 )
@@ -15,21 +18,27 @@ BOOST_AUTO_TEST_CASE( test2 )
     BOOST_CHECK( 3==1 );
 }
 
-BOOST_AUTO_TEST_CASE( test3 )
-{
-    int num1 = 3, num2 = num1 - 7;
-    BOOST_CHECK( sum(num1, num2)==7 );
-}
-
 BOOST_AUTO_TEST_CASE( linspacetest )
 {
     double * range;
     int size = 10;
     range = new double[size];
     linspace(range, size, 0, 9); // Should make a set = {0, 1, ..., 9}
-    for (int ii = 0; ii < 10; ii++)
+    for (int ii = 0; ii < size; ii++)
     {
         BOOST_CHECK( range[ii] == ii );
+    }
+}
+
+BOOST_AUTO_TEST_CASE( logspacetest )
+{
+    double * range;
+    int size = 10;
+    range = new double[size];
+    logspace(range, size, 0, 9); // Should make a set = {1, 10, 100, 1000, ..., 10^9};
+    for (int ii = 0; ii < size; ii++)
+    {
+        BOOST_CHECK( range[ii] == pow(10,ii) );
     }
 }
 
