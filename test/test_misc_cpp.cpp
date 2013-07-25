4c5f5b1f01af3576cdf58843cc6ce0a8d84b768f
Andy Eschbacher <otto.hasselblad@gmail.com>
2013-07-25 15:21:37 -0400

    Added tests on rr()

--- test/test_misc_cpp.cpp
+++ test/test_misc_cpp.cpp
@@ -1,6 +1,6 @@
 // Compile with g++ -otest -lboost_unit_test_framework TestExample.cpp -L/opt/local/lib -I/opt/local/include
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE rrstdmp test
+#define BOOST_TEST_MODULE rrstdmp_test
 #include <boost/test/unit_test.hpp>
 #include <cmath>
 #include <iostream>
@@ -15,11 +15,6 @@
 BOOST_AUTO_TEST_CASE( test1 )
 {
     BOOST_CHECK_EQUAL( 2,2 );
-}
-
-BOOST_AUTO_TEST_CASE( test2 )
-{
-    BOOST_CHECK_EQUAL( 3,1 );
 }
 
 BOOST_AUTO_TEST_CASE( linspacetest )
@@ -57,16 +52,16 @@ BOOST_AUTO_TEST_CASE( cumSumNormtest )
     cumSumNorm(x,10,sum);
 
     for (int ii = 0; ii < 10; ii++)
-        BOOST_CHECK( abs(ans[ii] - x[ii]) < 1.0e-20 ); // Is this a sufficient condition?
+        BOOST_CHECK( fabs(ans[ii] - x[ii]) < 1.0e-10 );
 }
 
 int globalWindow = 100, globalOverlap = 50;
 double ge = 0.01, k = 0.97163540631/(2.0 * M_PI);
 const double TWOPI = 2.0 * M_PI;
 
-BOOST_AUTO_TEST_CASE( rr_test )
+BOOST_AUTO_TEST_CASE( rrInit_test )
 {
-    int size = 100, rrcntr;
+    int size = 100, rrcntr = 0;
     double x[size], y[size];
 
     // Case 1: k = 0; 4-periodic map
@@ -81,9 +76,35 @@ BOOST_AUTO_TEST_CASE( rr_test )
     stdmpInit(x,y);
     BOOST_CHECK_EQUAL(y[0],rrInit(x,y,rrcntr));
 
-    // Case 3: k = k_g; KAM Torus
-    k = 0.97163540631/(2.0 * M_PI);
-    x[0] = 0.0; y[0] = 
+    // Case 3: k = 10; randomly chosen initial conditions
+    k = 10.0;
+    x[0] = 0.112311; y[0] = M_PI / 10.0;
+    stdmpInit(x,y);
+    double rrCurr = rrInit(x,y,rrcntr);
+    BOOST_CHECK_MESSAGE(fabs(rrCurr - 0.01) < 1.0e-10, \
+                        "(rr - 0.01) = " << (rrCurr - 0.01));
+}
+
+BOOST_AUTO_TEST_CASE( rr_test )
+{
+    int size = 100, rrcntr = 0;
+    double x[size], y[size], rrCurr;
+
+    // case 1: k = 0; 4-periodic map
+    k = 0;
+    x[0] = 0.0; y[0] = 1.0 / 4.0;
+    stdmpInit(x,y);
+    rrCurr = rrInit(x,y,rrcntr);
+    BOOST_CHECK_MESSAGE(fabs(rrCurr - 0.25) < 1.0e-5, \
+                        "(rr - 0.01) = " << (rrCurr - 0.01));
+
+    for (int ii = 0; ii < 10; ii++)
+    {
+        stdmp(x,y);
+        rrCurr = rr(x,y,rrcntr);
+        BOOST_CHECK_MESSAGE(fabs(rrCurr - 0.25) < 1.0e-5, \
+                            "(rr - 0.01) = " << (rrCurr - 0.01));
+    }
 }
 
 //EOF
